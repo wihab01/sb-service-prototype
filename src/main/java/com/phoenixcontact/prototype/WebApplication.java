55faa723283aeaa0bd5ade4935b197496a56e3f0
Wilfried Habelitz <wilfried.habelitz@atos.net>
2018-07-05 15:56:05 +0200

    Transfer original file (with tomcat libs); go back to Java 8; rename artifact

--- src/main/java/com/phoenixcontact/prototype/WebApplication.java
+++ src/main/java/com/phoenixcontact/prototype/WebApplication.java
@@ -2,11 +2,18 @@ package com.phoenixcontact.prototype;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 public class WebApplication extends SpringBootServletInitializer {
-    public static void main(String[] args) {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(Application.class);
+	}
+
+	public static void main(String[] args) {
         SpringApplication.run(WebApplication.class, args);
     }
 }
